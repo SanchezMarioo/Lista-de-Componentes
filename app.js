ab9cd0652377866125bf8d7d74f78171e9604139
SanchezMarioo <139809565+SanchezMarioo@users.noreply.github.com>
2024-06-12 13:47:13 +0200

    chore: Update npm dependencies and add toastr library

--- app.js
+++ app.js
@@ -1,82 +1,112 @@
-
 function procesadorSeleccionado() {
-let procesador = document.getElementById('procesador');
-let procesadorPrecio = document.getElementById('procesador-value');    
-let imagenUrl = document.getElementById('procesadorUrl');
-document.querySelector('select').addEventListener('change', function(event) {
-    switch (event.target.value) {
-    case 'intel-i9-590':
-         procesadorPrecio.value="590$";
-         imagenUrl.src = "https://www.intel.com/content/dam/www/central-libraries/xa/en/images/intel-core-i9-badge-1920x1080.png";
-         imagenUrl.alt = "Intel i9 590";
-         imagenUrl.width = "400";
-        break;
-    case 'intel-i7-380':
-        procesadorPrecio.value="380$";
-        imagenUrl.src = "https://www.intel.com/content/dam/www/central-libraries/xl/es/images/2022-06/km-1920x1080-i7.png";
-        break;
-    case 'intel-i5-250':
-        procesadorPrecio.value="250$";
-        imagenUrl.src = "https://www.intel.com/content/dam/www/central-libraries/xl/es/images/2022-06/km-1920x1080-i5.png";
-        break;
-    case 'intel-i3-120':
-        procesadorPrecio.value="120$";
-        imagenUrl.src = "https://www.intel.com/content/dam/www/central-libraries/xl/es/images/2022-06/km-1920x1080-i3.png";
-        break;
-    case 'amd-ryzen9-550':
-        procesadorPrecio.value="550$";
-        imagenUrl.src = "https://www.vektrapc.com/cdn/shop/files/1_25a7388f-c71c-4b1e-a189-1b68408f834c.webp?v=1684308927";
-        break;
-    case 'amd-ryzen7-350':
-        procesadorPrecio.value="350$";
-        imagenUrl.src = "https://i.ebayimg.com/images/g/-pQAAOSwms5kQ7YX/s-l1200.webp";
-        break;
-    case 'amd-ryzen5-220':
-        procesadorPrecio.value="220$";
-        imagenUrl.src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQEJ2BMDDw4vXGwssbzSXOiEss7aH-WyHjD4w&s";
-        break;
-    case 'amd-ryzen3-100':
-        procesadorPrecio.value="100$";
-        imagenUrl.src = "https://www.amd.com/system/files/82446-raven-am4-ryzen-3-pib-left-facing-1260x709.png";
-        break;
-    case 'apple-m1-700':
-        procesadorPrecio.value="700$";
-        imagenUrl.src = "https://www.apple.com/newsroom/images/product/mac/standard/Apple_M1-Pro-M1-Max_Chips_10182021_big.jpg.slideshow-xlarge_2x.jpg";
-        break;
-    case 'apple-m2-1000':
-        procesadorPrecio.value="1000$";
-        imagenUrl.src = "https://www.apple.com/newsroom/images/live-action/wwdc-2023/standard/m2/Apple-WWDC23-M2-Ultra-chip-230605_big.jpg.large.jpg";
-        break;
-    case '0':
-        procesadorPrecio.value="Precio no disponible";
-        imagenUrl.src = "img/Procesador.png";
-        break;
-    default:
-        mensaje = 'Por favor, seleccione un procesador válido.';
-        break;
-    dragDrop(procesadorPrecio);
-    
-}
-});
-}
-procesadorSeleccionado();
-function dragDrop() {
-    let procesadorValue = document.getElementById('procesadorUrl');
-    let procesadorDrag = document.getElementById('procesador-drag');
+    const procesador = document.getElementById('procesador');
+    const imagenUrl = document.getElementById('procesadorUrl');
+    const procesadorValue = document.getElementById('procesador-value');
+    const procesadorDrag = document.getElementById('procesador-drag');
 
-    // Cuando se inicia el arrastre, guarda el valor en el objeto de datos de arrastre
-    procesadorValue.addEventListener('dragstart', function(event) {
-        event.dataTransfer.setData('text/plain', procesadorValue.value);
+    procesador.addEventListener('change', function(event) {
+        let procesadorPrecio;
+        switch (event.target.value) {
+            case 'intel-i9-590':
+                procesadorPrecio = "590$";
+                imagenUrl.src = "https://www.intel.com/content/dam/www/central-libraries/xa/en/images/intel-core-i9-badge-1920x1080.png";
+                imagenUrl.alt = "Intel i9 590";
+                break;
+            case 'intel-i7-380':
+                procesadorPrecio = "380$";
+                imagenUrl.src = "https://www.intel.com/content/dam/www/central-libraries/xl/es/images/2022-06/km-1920x1080-i7.png";
+                imagenUrl.alt = "Intel i7 380";
+                break;
+            case 'intel-i5-250':
+                procesadorPrecio = "250$";
+                imagenUrl.src = "https://www.intel.com/content/dam/www/central-libraries/xl/es/images/2022-06/km-1920x1080-i5.png";
+                imagenUrl.alt = "Intel i5 250";
+                break;
+            case 'intel-i3-120':
+                procesadorPrecio = "120$";
+                imagenUrl.src = "https://www.intel.com/content/dam/www/central-libraries/xl/es/images/2022-06/km-1920x1080-i3.png";
+                imagenUrl.alt = "Intel i3 120";
+                break;
+            case 'amd-ryzen9-550':
+                procesadorPrecio = "550$";
+                imagenUrl.src = "https://www.vektrapc.com/cdn/shop/files/1_25a7388f-c71c-4b1e-a189-1b68408f834c.webp?v=1684308927";
+                imagenUrl.alt = "AMD Ryzen 9 550";
+                break;
+            case 'amd-ryzen7-350':
+                procesadorPrecio = "350$";
+                imagenUrl.src = "https://i.ebayimg.com/images/g/-pQAAOSwms5kQ7YX/s-l1200.webp";
+                imagenUrl.alt = "AMD Ryzen 7 350";
+                break;
+            case 'amd-ryzen5-220':
+                procesadorPrecio = "220$";
+                imagenUrl.src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQEJ2BMDDw4vXGwssbzSXOiEss7aH-WyHjD4w&s";
+                imagenUrl.alt = "AMD Ryzen 5 220";
+                break;
+            case 'amd-ryzen3-100':
+                procesadorPrecio = "100$";
+                imagenUrl.src = "https://www.amd.com/system/files/82446-raven-am4-ryzen-3-pib-left-facing-1260x709.png";
+                imagenUrl.alt = "AMD Ryzen 3 100";
+                break;
+            case 'apple-m1-700':
+                procesadorPrecio = "700$";
+                imagenUrl.src = "https://images.versus.io/objects/apple-m1-pro-10-core.front.variety.1635345805946.jpg";
+                imagenUrl.alt = "Apple M1 700";
+                break;
+            case 'apple-m2-1000':
+                procesadorPrecio = "1000$";
+                imagenUrl.src = "https://www.apple.com/newsroom/images/live-action/wwdc-2023/standard/m2/Apple-WWDC23-M2-Ultra-chip-230605_big.jpg.large.jpg";
+                imagenUrl.alt = "Apple M2 1000";
+                break;
+            case '0':
+                procesadorPrecio = "Precio no disponible";
+                imagenUrl.src = "img/Procesador.png";
+                imagenUrl.alt = "Procesador por defecto";
+                break;
+            default:
+                procesadorPrecio = "Seleccione un procesador válido";
+                imagenUrl.src = "img/Procesador.png";
+                imagenUrl.alt = "Procesador por defecto";
+                break;
+        }
+
+        procesadorValue.value = procesadorPrecio;
     });
 
-    // Cuando se arrastra sobre el objetivo, evita el comportamiento predeterminado para permitir soltar
+    dragDrop(imagenUrl, procesadorDrag);
+}
+
+
+function dragDrop(imagenUrl, procesadorDrag) {
+    imagenUrl.addEventListener('dragstart', function(event) {
+        event.dataTransfer.setData('text', event.target.id);
+    });
+
     procesadorDrag.addEventListener('dragover', function(event) {
         event.preventDefault();
     });
 
-    // Cuando se suelta, actualiza el valor del objetivo con los datos de arrastre
     procesadorDrag.addEventListener('drop', function(event) {
         event.preventDefault();
-        procesadorDrag.value = event.dataTransfer.getData('text/plain');
+        mostrarMensajeAñadidoCesta();
     });
+    }
+
+function mostrarMensajeAñadidoCesta() {
+    const mensaje = document.createElement('div');
+    mensaje.textContent = "Producto añadido a la cesta";
+    mensaje.style.position = 'fixed';
+    mensaje.style.bottom = '20px';
+    mensaje.style.right = '20px';
+    mensaje.style.backgroundColor = 'green';
+    mensaje.style.color = 'white';
+    mensaje.style.padding = '10px';
+    mensaje.style.boxShadow = '0 0 10px rgba(0, 0, 0, 0.1)';
+    mensaje.style.zIndex = '1000';
+    document.body.appendChild(mensaje);
+
+    setTimeout(() => {
+        mensaje.remove();
+    }, 3000);
 }
+
+document.addEventListener('DOMContentLoaded', procesadorSeleccionado);
